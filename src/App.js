50ae81747ff023f18e440629b0894196f7f4702b
c23fk <57362551+c23fk@users.noreply.github.com>
2023-04-26 14:07:04 -0400

    grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Component } from "react";
 
 function App() {
   return (
-    <div>jalkj</div>
+    <div>
+      
+    </div>
   );
 }
 
+function Grid(){
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  return(
+      <table>
+          {days.map((day) => (
+            <tr>  
+              <td>{day}</td>
+            </tr>
+          ))
+          }
+      </table>
+  )
+}
 export default App;
