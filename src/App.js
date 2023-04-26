394f7b52e9b89bf6af083830e6281d9f54949424
c23fk <57362551+c23fk@users.noreply.github.com>
2023-04-26 14:56:41 -0400

    added dates and times

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import { Component } from "react";
 
 function App() {
   return (
     <div>
-      
+      <Grid/>
     </div>
   );
 }
 
 function Grid(){
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const times = ['10:00', '10:30', '11:00','11:30', '12:00', '1:30', '2:00','2:30', '3:00', '3:30'];
 
   return(
       <table>
+          <tr>
           {days.map((day) => (
-            <tr>  
-              <td>{day}</td>
-            </tr>
+              <th>{day}</th>
           ))
           }
+          </tr>
+        {
+        times.map((time) => (
+          <tr>
+            {days.map(() => (
+            <TimeSlot time={time}/>
+            ))
+            }
+          </tr>
+        ))
+        }
       </table>
+  )
+}
+
+function TimeSlot(props){
+  return(
+      <td>
+        <div className = "time">
+          {props.time}
+        </div>
+      </td>
   )
 }
 export default App;
